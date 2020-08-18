7755418c7dc2fc501ef5dced7d2a2a6eb10e101b
汪浩 <wanghao@wuzhi-ai.com>
2020-08-18 09:17:13 +0800

    Add timeout & dedup logic to augmentation.

--- tools/augment.py
+++ tools/augment.py
@@ -9,6 +9,7 @@ import imgaug as ia
 import os
 from pascal_voc_writer import Writer
 import sequence
+import timeout_decorator
 import xml.etree.ElementTree as ET
 
 
@@ -48,42 +49,58 @@ def augment(augment_size=None):
         source_root = stree.getroot()
         img_name = source_root.find('./filename').text
 
+        image = cv2.imread('%s/%s' % (FLAGS.image_source_dir, img_name))
+
+        _bbs = []
+        for obj in source_root.findall('./object'):
+            bbox = obj.find('bndbox')
+            bb = ia.BoundingBox(x1=int(float(bbox.find('xmin').text)),
+                                y1=int(float(bbox.find('ymin').text)),
+                                x2=int(float(bbox.find('xmax').text)),
+                                y2=int(float(bbox.find('ymax').text)),
+                                label=obj.find('name').text)
+            _bbs.append(bb)
+
+        bbs = ia.BoundingBoxesOnImage(_bbs, shape=image.shape)
+
         for i in range(aug_size):
-            sp = img_name.split('.')
-            img_outfile = '%s/%s-%02d.%s' % (FLAGS.image_output_dir, sp[0], i, sp[-1])
-            xml_outfile = '%s/%s-%02d.xml' % (FLAGS.annot_output_dir, sp[0], i)
+            try:
+                generate_one_aug(image, img_name, bbs, source_root, i, seq)
+            except Exception as e:
+                print(str(e))
+                pass
 
-            seq_det = seq.to_deterministic()
 
-            image = cv2.imread('%s/%s' % (FLAGS.image_source_dir, img_name))
-            _bbs = []
-            for obj in source_root.findall('./object'):
-                bbox = obj.find('bndbox')
-                bb = ia.BoundingBox(x1=int(float(bbox.find('xmin').text)),
-                                    y1=int(float(bbox.find('ymin').text)),
-                                    x2=int(float(bbox.find('xmax').text)),
-                                    y2=int(float(bbox.find('ymax').text)),
-                                    label=obj.find('name').text)
-                _bbs.append(bb)
+@timeout_decorator.timeout(15)
+def generate_one_aug(image, img_name, bbs, source_root, i, seq):
+    sp = img_name.split('.')
 
-            bbs = ia.BoundingBoxesOnImage(_bbs, shape=image.shape)
+    img_outfile = '%s/%s-%02d.%s' % (FLAGS.image_output_dir, sp[0], i, sp[-1])
+    xml_outfile = '%s/%s-%02d.xml' % (FLAGS.annot_output_dir, sp[0], i)
 
-            image_aug = seq_det.augment_images([image])[0]
-            bbs_aug = seq_det.augment_bounding_boxes(
-                [bbs])[0].remove_out_of_image().clip_out_of_image()
+    if os.path.exists(img_outfile) and os.path.exists(xml_outfile):
+        print("%s exists" % img_outfile)
+        return
+    else:
+        print("Generating %s" % img_outfile)
 
-            writer = Writer(img_outfile,
-                            int(float(source_root.find('size').find('width').text)),
-                            int(float(source_root.find('size').find('height').text)))
-            for bb in bbs_aug.bounding_boxes:
-                writer.addObject(bb.label,
-                                 int(bb.x1),
-                                 int(bb.y1),
-                                 int(bb.x2),
-                                 int(bb.y2))
+    seq_det = seq.to_deterministic()
+    image_aug = seq_det.augment_images([image])[0]
+    bbs_aug = seq_det.augment_bounding_boxes(
+        [bbs])[0].remove_out_of_image().clip_out_of_image()
 
-            cv2.imwrite(img_outfile, image_aug)
-            writer.save(xml_outfile)
+    writer = Writer(img_outfile,
+                    int(float(source_root.find('size').find('width').text)),
+                    int(float(source_root.find('size').find('height').text)))
+    for bb in bbs_aug.bounding_boxes:
+        writer.addObject(bb.label,
+                         int(bb.x1),
+                         int(bb.y1),
+                         int(bb.x2),
+                         int(bb.y2))
+
+    cv2.imwrite(img_outfile, image_aug)
+    writer.save(xml_outfile)
 
 
 def main(_argv):
